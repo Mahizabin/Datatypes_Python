6e2ac736315451c289db0f5082304518fc3ded73
Mahizabin <123229709+Mahizabin@users.noreply.github.com>
2023-06-17 11:58:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+a=564812184321585215218491218
+print(type(a))
+
+b="""564812184321585215218491218"""
+print(b)
+print(type(b))
+
+c=5648121843215852.15218491218
+print(type(c))
+
+d=complex(5,9)
+print(d)
+print(type(d))
+
+e=True
+print(e)
+print(type(e))
+
+f=None
+print(f)
+print(type(f))
+
+
+#lists can be accssed and modified
+g=["Mahi",1,2.3,None,True]
+print(g)
+print(type(g))
+print(g[0])
+g[0]="Mahizabin"
+print(g)
+
+
+#tuples can be accessed but not modified
+h=("Mahi",1,2.3,-45.6,True,None)
+print(h)
+print(type(h))
+print(h[0])
+
+#dictionary is a set of key value pairs
+dict={"Aspiring":"directing one's hopes or ambitions towards becoming a specified type of person","friend":"Relationship between people who have mutual affection for each other","age":23}
+print(dict)
+
+
